9c0dfbc62e9e15e1cb06e90f6b39430cb73c13b6
Jim Pruyne <pruyne@uchicago.edu>
2022-11-30 12:47:05 -0600

    Add more to requirements.txt to get imports to work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,12 +14,7 @@ import os
 import sys
 
 sys.path.insert(0, os.path.abspath("../../src/"))
-autodoc_mock_imports = [
-    "sqlalchemy",
-    "sqlmodel",
-    ".models",
-    ".gen_tools",
-]
+autodoc_mock_imports = []
 
 autodoc_typehints = "description"
 
