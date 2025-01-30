211f9f5539fa3c63325272bf5cfae3fbbbfe8b7c
Justin Wozniak <woz@anl.gov>
2025-01-29 19:06:21 -0700

    Better pkg list

--- docs/setup.sh
+++ docs/setup.sh
@@ -9,10 +9,20 @@ which pip
 echo "setup.sh: confirm?"
 read _
 
+PKGS=(
+  # poetry
+  furo
+  sphinx
+  sphinx-copybutton
+  sqlmodel
+  SQLAlchemy
+)
+
 (
   set -x
-  pip install poetry sphinx
-  poetry install
+  # sphinx-copybutton is in requirements.txt
+  pip install ${PKGS[@]}
+  # poetry install
 )
 
 echo "setup.sh: OK"
